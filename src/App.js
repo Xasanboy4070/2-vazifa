f906fe4bf7aa8686df34f480c37a5fe858c48e10
xasanboy <xasanboyorifjonov2@gmail.com>
2022-07-30 06:55:15 +0500

    finish code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    count: 0,
+    number: 0,
+    seconds: 0,
+    disabledDec: false,
+    disabledInc: false,
+  };
+  handleNumber(e) {
+    this.setState({ number: Number(e.target.value) });
+  }
+  handleSeconds(e) {
+    this.setState({ seconds: Number(e.target.value) });
+  }
+
+  handleDec = () => {
+    this.setState({ disabledDec: true });
+    setTimeout(() => {
+      this.setState({ count: this.state.count + this.state.number });
+      console.log("Inc click");
+      this.setState({ disabledDec: false });
+    }, Number(this.state.seconds * 1000));
+  };
+
+  handleInc = () => {
+    this.setState({ disabledInc: true });
+    setTimeout(() => {
+      this.setState({ count: this.state.count - this.state.number });
+      console.log("Inc click");
+      this.setState({ disabledInc: false });
+    }, Number(this.state.seconds * 1000));
+  };
+
+  render() {
+    return (
+      <>
+        <div className="inputs">
+          <input
+            type="number"
+            placeholder="seconds"
+            onChange={(e) => {
+              this.handleSeconds(e);
+            }}
+          />
+          <input
+            type="number"
+            placeholder="number"
+            onChange={(e) => {
+              this.handleNumber(e);
+            }}
+          />
+        </div>
+        <div className="btns">
+          <div>
+            <div className="anime">
+              <div className="colorDec animeDec"></div>
+            </div>
+            <button
+              disabled={this.state.disabledDec ? true : false}
+              onClick={this.handleDec}
+              style={{
+                backgroundColor: this.state.disabledDec ? "red" : "blue",
+              }}
+            >
+              Dec
+            </button>
+          </div>
+          <span>{this.state.count}</span>
+          <div>
+            <div className="anime">
+              <div className="colorInc animeInc"></div>
+            </div>
+            <button
+              disabled={this.state.disabledInc ? true : false}
+              onClick={this.handleInc}
+              style={{
+                backgroundColor: this.state.disabledInc ? "red" : "blue",
+              }}
+            >
+              Inc
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
